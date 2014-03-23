c1408e99991e833021f748babfa9aa7d6abed7f3
marumichannel <marumichannel@gmail.com>
2014-03-23 14:10:25 -0400

    Move strings to separate file

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -1,68 +1,6 @@
 var currentStrings
-var stringDictionary = { strings: [
-    {
-        "id": "yukachi",
-        "2048": "井口 2048",
-        "Title": "井口",
-        "Subtitle": "(Iguchi Yuka)",
-        "Join the numbers and get to the 2048 tile!": "Join the 井口 and get the 井口 2048 tile!",
-        "When two tiles with the same number touch": "When two tiles with the same 井口 touch, they merge into one!",
-        "you join tiles to score high!": "you join 井口 to score high!",
-        "You win!": "You win! ⊂（＿＾ω＾）⊃",
-        "Game over!": "Game over!",
-        "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#yukachi"
-    },
-    {
-        "id": "rieshon",
-        "2048": "りえしょん 2048",
-        "Title": "りえしょん",
-        "Subtitle": "(Murakawa Rie)",
-        "Join the numbers and get to the 2048 tile!": "Join the りえしょん and get the りえしょん 2048 tile!",
-        "When two tiles with the same number touch": "When two tiles with the same りえしょん touch",
-        "you join tiles to score high!": "you join りえしょん to score high!",
-        "You win!": "You win! (*´ω`*三*´ω`*)",
-        "Game over!": "Game over!",
-        "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#rieshon"
-    },
-    {
-        "id": "ayaneru",
-        "2048": "あやねる 2048",
-        "Title": "あやねる",
-        "Subtitle": "(Sakura Ayane)",
-        "Join the numbers and get to the 2048 tile!": "Join the あやねる and get the あやねる 2048 tile!",
-        "When two tiles with the same number touch": "When two tiles with the same あやねる touch",
-        "you join tiles to score high!": "you join あやねる to score high!",
-        "You win!": "You win!",
-        "Game over!": "Game over!",
-        "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#ayaneru"
-    },
-    {
-        "id": "ucchii",
-        "2048": "うっちー 2048",
-        "Title": "うっちー",
-        "Subtitle": "(Uchida Aya)",
-        "Join the numbers and get to the 2048 tile!": "Join the うっちー and get the うっちー 2048 tile!",
-        "When two tiles with the same number touch": "When two tiles with the same うっちー touch",
-        "you join tiles to score high!": "you join うっちー score high!",
-        "You win!": "You win!　（・８・）",
-        "Game over!": "Game over!",
-        "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#ucchii"
-    },
-    {
-        "id": "sugar",
-        "2048": "しゅがぁ 2048",
-        "Title": "しゅがぁ",
-        "Subtitle": "(Satou Satomi)",
-        "Join the numbers and get to the 2048 tile!": "Join the しゅがぁ and get the しゅがぁ 2048 tile!",
-        "When two tiles with the same number touch": "When two tiles with the same しゅがぁ touch",
-        "you join tiles to score high!": "you join しゅがぁ to score high!",
-        "You win!": "You win!",
-        "Game over!": "Game over!",
-        "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#sugar"
-    }
-]}
 
-function getStrings(style) {
+function getStrings(style, stringDictionary) {
     for (var index = 0; index < stringDictionary.strings.length; index++) {
         if (style == stringDictionary.strings[index].id) {
             return stringDictionary.strings[index]
@@ -96,8 +34,8 @@ function tweetButton() {
     return tweet;
 }
 
-function initStyle(style) {
-    currentStrings = getStrings(style);
+function initStyle(style, stringDictionary) {
+    currentStrings = getStrings(style, stringDictionary);
     changeStrings(currentStrings);
     document.querySelector("#theme").href = 'style/' + style + '.css';
     if (typeof ga !== "undefined") {
@@ -134,10 +72,11 @@ function createButtons(stringDictionary) {
     }
 }
 
-if (typeof getStrings(location.hash.replace("#", "")) != "undefined") {
-    initStyle(location.hash.replace("#", ""))
-    createButtons(stringDictionary);
+var stringDict = iguchi2048_stringDictionary
+if (typeof getStrings(location.hash.replace("#", ""), stringDict) != "undefined") {
+    initStyle(location.hash.replace("#", ""), stringDict)
+    createButtons(stringDict);
 } else {
-    changeStyle(stringDictionary.strings[0].id)
+    changeStyle(stringDict.strings[0].id)
 };
 
