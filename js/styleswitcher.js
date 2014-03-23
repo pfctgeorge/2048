0370619efb5852ede989fba0cced6ec643e4cbd0
marumichannel <marumichannel@gmail.com>
2014-03-23 11:57:10 -0400

    Fix spaces

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -4,7 +4,7 @@ var styleDictionary = {
         "2048": "井口 2048",
         "Join the numbers and get to the 2048 tile!": "Join the 井口 and get the 井口 2048 tile!",
         "When two tiles with the same number touch": "When two tiles with the same 井口 touch, they merge into one!",
-        "you join tiles to score high!": "you join 井口　to score high!",
+        "you join tiles to score high!": "you join 井口 to score high!",
         "You win!": "You win! ⊂（＿＾ω＾）⊃",
         "Game over!": "Game over!",
         "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#yukachi"
@@ -13,7 +13,7 @@ var styleDictionary = {
         "2048": "りえしょん 2048",
         "Join the numbers and get to the 2048 tile!": "Join the りえしょん and get the りえしょん 2048 tile!",
         "When two tiles with the same number touch": "When two tiles with the same りえしょん touch",
-        "you join tiles to score high!": "you join りえしょん　to score high!",
+        "you join tiles to score high!": "you join りえしょん to score high!",
         "You win!": "You win! (*´ω`*三*´ω`*)",
         "Game over!": "Game over!",
         "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#rieshon"
@@ -22,7 +22,7 @@ var styleDictionary = {
         "2048": "あやねる 2048",
         "Join the numbers and get to the 2048 tile!": "Join the あやねる and get the あやねる 2048 tile!",
         "When two tiles with the same number touch": "When two tiles with the same あやねる touch",
-        "you join tiles to score high!": "you join あやねる　to score high!",
+        "you join tiles to score high!": "you join あやねる to score high!",
         "You win!": "You win!",
         "Game over!": "Game over!",
         "http://marumichannel.github.io/2048/": "http://marumichannel.github.io/2048/#ayaneru"
